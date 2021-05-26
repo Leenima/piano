2a1c2fa55deba72bfeccaffd5a3b03017aaf48dc
Leenima <ggfggfggf123@naver.com>
2021-05-26 10:41:50 +0900

    기능 추가?

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,15 @@
 <template>
 <div>
-  <h1>피아노의 모든 것 </h1>
-  <h2>88개 내외의 건반이 있고, 이를 누르면 연결된 해머가 각 현을 때려서 낸 소리를 공명 상자로 증폭하는 건반 악기.</h2>
+  <section class="hero is-primary">
+    <dev class="hero-body">
+      <p class="title">
+        피아노
+      </p>
+      <p class="subtitle">
+        피아노의 모든 것
+      </p>
+    </dev>
+  </section>
+
 </div>
 </template>

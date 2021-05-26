560dde2a8f4fbd1a4e093a269cc1aebeac93db45
Leenima <ggfggfggf123@naver.com>
2021-05-26 11:06:05 +0900

    텍스트 변경

--- pages/index.vue
+++ pages/index.vue
@@ -5,10 +5,10 @@
   <section class="hero is-primary">
     <dev class="hero-body">
       <p class="title">
-        피아노<i class="fas fa-piano"></i>
+        피아노의 모든것<i class="fab fa-odnoklassniki-square"></i>
       </p>
       <p class="subtitle">
-        피아노의 모든 것
+        88개 내외의 건반이 있고, 이를 누르면 연결된 해머가 각 현을 때려서 낸 소리를 공명 상자로 증폭하는 건반 악기.
       </p>
     </dev>
   </section>

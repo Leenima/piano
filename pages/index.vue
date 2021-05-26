b5c1e5ce1f06dd333336b0a04e115d7a42ae3b0e
Leenima <ggfggfggf123@naver.com>
2021-05-26 10:45:36 +0900

    아이콘 추가

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,11 @@
+<!-- 흠 -->
+
 <template>
 <div>
   <section class="hero is-primary">
     <dev class="hero-body">
       <p class="title">
-        피아노
+        피아노<i class="fas fa-piano"></i>
       </p>
       <p class="subtitle">
         피아노의 모든 것

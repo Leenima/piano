7380787a90a74fcd3af4f59985f56cab0414e233
Leenima <ggfggfggf123@naver.com>
2021-06-09 11:57:41 +0900

    웹페이지 하나 더 추가

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,16 +2,27 @@
   <div>
     <nav class="breadcrumb">
       <ul>
-        <li><nuxt-link to="/"><i class="fas fa-home"></i>홈으로</nuxt-link></li>
+        <li>
+          <nuxt-link to="/">
+            <i class="fas fa-home" />홈으로
+          </nuxt-link>
+        </li>
       </ul>
       <li>
-        <nuxt-link to="/names"><i class="fas fa-palette"></i>피아노 색 정하기</nuxt-link>
+        <nuxt-link to="/instrument1">
+          <i class="fas fa-palette" />악기 정하기1
+        </nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/instrument2">
+          <i class="fas fa-palette" />악기 정하기2
+        </nuxt-link>
       </li>
     </nav>
     <Nuxt />
     <footer class="footer">
       <div class="content has-text-centered">
-        <i class ="fas fa-copyright"></i> 2020-2021, 모든 저작권은 <strong>피아노의 모든것</strong>에 있습니다.
+        <i class="fas fa-copyright" />2020-2021, 모든 저작권은 <strong>악기의 모든것</strong>에 있습니다.
       </div>
     </footer>
   </div>
---
+++ pages/instrument2.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <section class="hero is-link">
+      <div class="hero-body">
+        <p class="title">
+          <i class="fas fa-glass-martini-alt" /> 악기의
+          <i class="fas fa-glass-cheers" /> 모든것 2
+        </p>
+        <p class="subtitle">
+          악기를 골라주세요
+        </p>
+      </div>
+    </section>
+
+    <hr>
+    <section class="columns" />
+    <div class="colums" />
+    <table class="table">
+      <thead>
+        <tr>
+          <th>번호</th>
+          <th>종류</th>
+          <th>악기 이름</th>
+        </tr>
+      </thead>
+      <tbody>
+        <template v-for="pos in tableinstrument.length">
+          <tr :key="pos">
+            <td>{{ pos }}</td>
+            <td>{{ tableinstrument[pos - 1].family }}</td>
+            <td>{{ tableinstrument[pos - 1].instrument }}</td>
+          </tr>
+        </template>
+      </tbody>
+    </table>
+    <div class="colums" />
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  async asyncData () {
+    const instrumentt = await axios.get('https://raw.githubusercontent.com/mobyvb/midi-converter/master/lib/instruments.json')
+    alert(Object.keys(instrumentt.data[0]))
+    return {
+      tableinstrument: instrumentt.data,
+      tableinstrumentkey: Object.keys(instrumentt.data[0]
+      )
+    }
+  }
+}
+</script>
